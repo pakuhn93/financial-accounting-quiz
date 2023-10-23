058cddaef77394df995ab4a880c04e9d87ecbbbc
pakuhn <pakuhn93@gmail.com>
2023-10-23 15:21:41 -0700

    added home link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header(){
     return (
         <header>
+            <Link to="/">Home</Link>
             <Link to="/practice">Practice</Link>
         </header>
     );
